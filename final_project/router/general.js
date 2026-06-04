cd8981fce9b942b311ce8cc5ce5a7c8b0fcd68fd
daani786 <m.adnan.masood@gmail.com>
2026-06-04 07:59:14 -0400

    getbooksbyISBN endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,17 +12,34 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',async function (req, res) {
-  //Write your code here
-  //  return res.status(300).json({message: "Yet to be implemented"});
-  let data = await Promise.resolve(books);
-  data = JSON.stringify(data,null,4);
-  return res.status(200).send(data);
+    try {
+        //Write your code here
+        //  return res.status(300).json({message: "Yet to be implemented"});
+        let data = await Promise.resolve(books);
+        return res.status(200).send(JSON.stringify(data,null,4));
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+    try {
+        const isbn = parseInt(req.params.isbn, 10);
+        const book = await new Promise((resolve, reject) => {
+            const result = books[isbn];
+            if (result) {
+                resolve(result);
+            } else {
+                reject(new Error("Book not found"));
+            }
+        });
+        return res.status(200).send(JSON.stringify(book,null,4));
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
  });
   
 // Get book details based on author
